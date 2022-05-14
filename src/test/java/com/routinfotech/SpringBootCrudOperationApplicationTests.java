a41b24801ed7734293d618aedb2d8f8a134e3939
Ashutosh <ashutosh@routinfotech.com>
2022-05-14 21:49:13 +0530

    file modified

---
+++ src/test/java/com/routinfotech/SpringBootCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routinfotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
